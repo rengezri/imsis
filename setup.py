1aa92133256da1cc8d06c2c4caea2809f35f9b3b
rengezri <95188879+rengezri@users.noreply.github.com>
2021-11-30 23:57:12 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
+import setuptools
 
-setup(name='imsis',
-      version='1.0',
-      description='imsis',
-      classifiers=[
-        'Development Status :: 0.1',
-        'Programming Language :: Python :: 3.x',
-      ],
-      keywords='simple imaging and analysis in Python',
-      author='AA',
-      packages=find_packages(),
-      install_requires=[
-          'markdown',
-      ],
-      include_package_data=True,
-      zip_safe=False)
+setup(
+  name = 'imsis',
+  packages = ['imsis'],
+  version = '1.0',
+  license='MIT',
+  description = 'image analysis in Python',
+  author = 'rengezri',
+  author_email = 'rengezri@gmail.com',
+  url = 'https://github.com/rengezri',
+  download_url = 'https://github.com/rengezri/imsis/archive/refs/tags/v1.0.0.tar.gz',
+  keywords = ['image analysis', 'dialogs', 'batch processing'],   # Keywords that define your package best
+  install_requires=['markdown'],
+  classifiers=[
+    'Development Status :: 5 - Production',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
